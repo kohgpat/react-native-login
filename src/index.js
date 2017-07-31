ab4568fc473c78c0b0a9fc4cc11aedc5919b162c
Nikolay Burlov <kohgpat@gmail.com>
2017-07-31 16:33:23 +0800

    Add token refreshing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { AsyncStorage, Linking } from 'react-native';
+import {AsyncStorage, Linking} from 'react-native';
 import * as querystring from 'query-string';
 import uuid from 'react-native-uuid';
 import {decodeToken} from './util';
@@ -34,19 +34,50 @@ class Login {
     return this.tokenStorage.loadTokens();
   }
 
+  updateToken(tokens) {
+    if (!tokens) {
+      return;
+    }
+
+    const {redirect_uri, client_id} = this.conf;
+    const url = this.getRealmURL() + '/protocol/openid-connect/token';
+
+    const headers = new Headers();
+    headers.set('Content-Type', 'application/x-www-form-urlencoded');
+
+    const body = querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: tokens.refresh_token,
+      client_id: encodeURIComponent(client_id),
+    });
+
+    fetch(url, {method: 'POST', headers, body}).then(response => {
+      response.json().then(json => {
+        if (json.error) {
+          this.state.reject(json);
+        } else {
+          this.tokenStorage.saveTokens(json);
+          this.state.resolve(json);
+        }
+      });
+    });
+  }
+
   start(conf) {
     this.conf = conf;
-    return new Promise(function(resolve, reject) {
-      const {url, state} = this.getLoginURL();
-      this.state = {
-        ...this.state,
-        resolve,
-        reject,
-        state,
-      };
+    return new Promise(
+      function(resolve, reject) {
+        const {url, state} = this.getLoginURL();
+        this.state = {
+          ...this.state,
+          resolve,
+          reject,
+          state,
+        };
 
-      Linking.openURL(url);
-    }.bind(this));
+        Linking.openURL(url);
+      }.bind(this),
+    );
   }
 
   end() {
@@ -54,9 +85,9 @@ class Login {
   }
 
   onOpenURL(event) {
-    if(event.url.startsWith(this.conf.appsite_uri)) {
+    if (event.url.startsWith(this.conf.appsite_uri)) {
       const {state, code} = querystring.parse(querystring.extract(event.url));
-      if(this.state.state === state) {
+      if (this.state.state === state) {
         this.retrieveTokens(code);
       }
     }
@@ -73,12 +104,12 @@ class Login {
       grant_type: 'authorization_code',
       redirect_uri,
       client_id,
-      code
+      code,
     });
 
     fetch(url, {method: 'POST', headers, body}).then(response => {
       response.json().then(json => {
-        if(json.error) {
+        if (json.error) {
           this.state.reject(json);
         } else {
           this.tokenStorage.saveTokens(json);
@@ -102,13 +133,16 @@ class Login {
     const {redirect_uri, client_id, kc_idp_hint} = this.conf;
     const response_type = 'code';
     const state = uuid.v4();
-    const url = this.getRealmURL() + '/protocol/openid-connect/auth?' + querystring.stringify({
-      kc_idp_hint,
-      redirect_uri,
-      client_id,
-      response_type,
-      state,
-    });
+    const url =
+      this.getRealmURL() +
+      '/protocol/openid-connect/auth?' +
+      querystring.stringify({
+        kc_idp_hint,
+        redirect_uri,
+        client_id,
+        response_type,
+        state,
+      });
 
     return {url, state};
   }
